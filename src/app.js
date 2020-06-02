355929dfa8fa0879716b0a7c3508877b9e36a6f1
Victor Rocha <vrsarmento@gmail.com>
2020-06-02 19:26:12 -0300

    Markdown-Editor moved to new directory Views | Textarea focus on delete

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react'
 import marked from 'marked'
-import MarkdownEditor from 'components/markdown-editor'
+import MarkdownEditor from 'views/markdown-editor'
 
 import './css/style.css'
 
@@ -40,6 +40,7 @@ class App extends Component {
     this.handleRemove = () => {
       localStorage.removeItem('md')
       this.setState({ value: '', isSaving: null })
+      this.textarea.focus()
     }
 
     this.handleSave = () => {
