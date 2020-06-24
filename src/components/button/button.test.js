80182d414a7e275350bd9acd7b125febf8a40b90
Victor Rocha <vrsarmento@gmail.com>
2020-06-24 20:46:35 -0300

    Added enzyme tests

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -1,33 +1,69 @@
 'use strict'
 
 import React from 'react'
-import Button from './index'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import Button from './index'
+
+const noop = () => null
 
 it('Should Button Default to match snapshot', () => {
   const tree = renderer
-    .create(<Button onClick={() => null}>Click me</Button>)
+    .create(<Button onClick={noop}>Click me</Button>)
     .toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 it('Should Button Success to match snapshot', () => {
   const tree = renderer
-    .create(<Button onClick={() => null} kind='success'>Click me</Button>)
+    .create(<Button onClick={noop} kind='success'>Click me</Button>)
     .toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 it('Should Button Info to match snapshot', () => {
   const tree = renderer
-    .create(<Button onClick={() => null} kind='info'>Click me</Button>)
+    .create(<Button onClick={noop} kind='info'>Click me</Button>)
     .toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 it('Should Button Danger to match snapshot', () => {
   const tree = renderer
-    .create(<Button onClick={() => null} kind='danger'>Click me</Button>)
+    .create(<Button onClick={noop} kind='danger'>Click me</Button>)
     .toJSON()
   expect(tree).toMatchSnapshot()
+})
+
+it('Button with prop kind = "success" should has class "-success"', () => {
+  const wrapper = shallow(
+    <Button onClick={noop} kind='success'>
+      Success
+    </Button>
+  )
+  expect(wrapper.hasClass('-success')).toBe(true)
+  expect(wrapper.hasClass('-danger')).toBe(false)
+  expect(wrapper.hasClass('-info')).toBe(false)
+})
+
+it('Button with prop kind = "danger" should has class "-danger"', () => {
+  const wrapper = shallow(
+    <Button onClick={noop} kind='danger'>
+      Danger
+    </Button>
+  )
+  expect(wrapper.hasClass('-danger')).toBe(true)
+  expect(wrapper.hasClass('-success')).toBe(false)
+  expect(wrapper.hasClass('-info')).toBe(false)
+})
+
+it('Button with prop kind = "info" should has class "-info"', () => {
+  const wrapper = shallow(
+    <Button onClick={noop} kind='info'>
+      Info
+    </Button>
+  )
+  expect(wrapper.hasClass('-info')).toBe(true)
+  expect(wrapper.hasClass('-success')).toBe(false)
+  expect(wrapper.hasClass('-danger')).toBe(false)
 })
